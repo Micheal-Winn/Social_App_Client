179c7f336151473d319727302f57815d8a066bc1
thantzinwin <thant.zin.winnnn@gmail.com>
2023-01-06 23:28:10 +0630

    start using material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { useState } from 'react'
-import { Routes,Navigate,Route } from 'react-router-dom';
-import HomePage from 'scences/homePage/home';
-import LoginPage from 'scences/loginPage/login';
-import ProfilePage from 'scences/profilePage/profile';
+import { Routes,  Route } from "react-router-dom";
+import HomePage from "./scences/homePage/HomePage";
+import LoginPage from "./scences/loginPage/LoginPage";
+import ProfilePage from "./scences/profilePage/ProfilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
-
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
-    <div className="App">
-     <Routes>
-        <Route path='/' element={<LoginPage/>} />
-        <Route path='/home' element={<HomePage/>} />
-        <Route path='/profile/:userId' element={<ProfilePage/>} />
-     </Routes>
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
